f42dc37583dcf2383b98b532b94eb82d0559f218
Guilherme Blanco <guilherme.blanco@iesb.edu.br>
2022-09-10 10:34:10 -0300

    USB e PCIE16X

--- src/main/java/inc/hardware/motherboard/PAsusMotherboard.java
+++ src/main/java/inc/hardware/motherboard/PAsusMotherboard.java
@@ -1,6 +1,6 @@
 package inc.hardware.motherboard;
 
-import inc.hardware.exception.VideoNotFoundException;
+import  inc.hardware.exception.VideoNotFoundException;
 import inc.hardware.interfaces.*;
 import inc.hardware.sound.Beep;
 
@@ -31,22 +31,61 @@ public class PAsusMotherboard implements Motherboard {
 
     @Override
     public void connectUsb(Usb peripheral) {
-
+        if(usb1 == null){
+            usb1 = new Usb();
+        } else if(usb2 == null){
+            usb2 = new Usb();
+        } else if(usb3 == null){
+            usb3 = new Usb();
+        }
     }
 
     @Override
     public void removeUsb(Usb slot) {
-
+        if(usb1 != null){
+            usb1 == null;
+        } if(usb2 != null){
+            usb2 == null;
+        } if(usb3 != null){
+            usb3 == null;
+        }
     }
 
     @Override
     public void connectPcie(PCIE16X peripheral) {
-
+        if(pcie1 == null){
+            pcie1 = new PCIE16X() {
+                @Override
+                public PeripheralType getType() {
+                    return null;
+                }
+            };
+        }else if(pcie2 == null){
+            pcie2 = new PCIE16X() {
+                @Override
+                public PeripheralType getType() {
+                    return null;
+                }
+            };
+        }else if(pcie3 == null){
+            pcie3 = new PCIE16X() {
+                @Override
+                public PeripheralType getType() {
+                    return null;
+                }
+            };
+        }
     }
 
     @Override
     public void removePcie(PCIE16X slot) {
-
+        if(pcie1 != null){
+            pcie1 == null;
+        } else if (pcie2 != null){
+            pcie2 == null;
+        } else if (pcie3 != null){
+            pcie3 == null;
+        }
     }
 
     @Override
