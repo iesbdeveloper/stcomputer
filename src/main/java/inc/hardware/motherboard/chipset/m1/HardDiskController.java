ad7aa45eb55e16ef05f504c01651d03d359aaf8e
Guilherme Blanco <guilherme.blanco@iesb.edu.br>
2022-09-17 14:49:28 -0300

    Algumas funções do chipset e arrumando o controlador de disco

--- src/main/java/inc/hardware/motherboard/chipset/m1/HardDiskController.java
+++ src/main/java/inc/hardware/motherboard/chipset/m1/HardDiskController.java
@@ -1,14 +1,5 @@
 package inc.hardware.motherboard.chipset.m1;
 
-
-public class M1Chipset impements {
-
-    private final sound beep;
-
-    private bootloade bootloader;
-    private final keyboardcontroller keyboardcontroller;
-
-
-
+public class HardDiskController {
 
 }
--- src/main/java/inc/hardware/motherboard/chipset/m1/M1Chipset.java
+++ src/main/java/inc/hardware/motherboard/chipset/m1/M1Chipset.java
@@ -1,4 +1,26 @@
 package inc.hardware.motherboard.chipset.m1;
 
 public class M1Chipset {
+
+    public void getFreeMemory(){
+    }
+    public void getTotalMemory(){
+    }
+    public void connectMemory(){
+    }
+    public void removeMemory(){
+    }
+    public void memoryStore(){
+    }
+    public void memoryLoad(){
+    }
+    public void checkMemory(){
+    }
+    public void checkSata(){
+    }
+    public void getSpecialKey(){
+    }
+    public void bootLoader(){
+    }
+
 }
