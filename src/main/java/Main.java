9075b5dfd037aeafdbaf6e2682568fa17a3660b9
EDDIE0virmond <eduvirmond@hotmail.com>
2022-10-07 22:06:37 -0300

    commit realizado pelo grupo como um todo, no caso aplicando uma resolucao da Main, ou seja, fiz o commit e tods as funcionalidades retornam e funcionam de acordo com seus devidos parametros e funcionalidades: espaco vazio, espaco total, read e write - correcao de uma saida de console a mais, agora com uma main, mais estruturada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,22 @@ import inc.hardware.so.FileSystem.No;
 import inc.hardware.storage.HardDisk10M;
 
 public class Main {
+    private static HardDisk10M disk10M = new HardDisk10M();
     public static void main(String[] args) {
-        HardDisk10M disk10M = new HardDisk10M();
+
         byte[] u = {0,0,0,1,1,1,1,1,1,0,1,1,0,1,1,0,0,1,0,1};
-        No<Long> nl = disk10M.write(u);
+        byte[] s = {0,0,0,1,1,1,1,0,1,0,1,0,1,0,1,0,1,1,1,0,1};
+        No<Long> l = disk10M.write(u);
+        No<Long> m = disk10M.write(s);
+        dbyte(l);
+        dbyte(m);
+        System.out.println("\nos espacos  \ntotal " + disk10M.espacoTotal() + " \nlivre " + disk10M.espacoLivre() );
+    }
+    public static void dbyte(No<Long> nl)
+    {
         for (byte o : disk10M.read(nl)) {
-            System.out.println(o);
-        }
-        System.out.println("fifi \n" + disk10M.espacoTotal() + " \n" + disk10M.espacoLivre() );
+        System.out.print(o + "");
+    }
+        System.out.println("\n");
     }
 }
