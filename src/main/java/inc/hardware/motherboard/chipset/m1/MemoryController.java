cfed861b9a7098a2102229f3e2a138bb060d5843
Bruno Lopes Bellinazo <bellinazobl@gmail.com>
2022-10-08 02:51:19 -0300

    ConnectMemory / Memorycontroller

--- src/main/java/inc/hardware/motherboard/chipset/m1/MemoryController.java
+++ src/main/java/inc/hardware/motherboard/chipset/m1/MemoryController.java
@@ -1,6 +1,7 @@
 package inc.hardware.motherboard.chipset.m1;
 
 import inc.hardware.interfaces.Memory;
+import inc.hardware.interfaces.Sata;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,28 @@ public class MemoryController {
 
     private int i;
 
-    public List<>
+    private Memory memorySlot1;
+    private Memory memorySlot2;
 
+    public List<Memory> getMemory() {
+        return memory;
+    }
+
+    public void setMemory(List<Memory> memory) {
+        this.memory = memory;
+    }
+
+    public void connectMemory(Memory peripheral){
+        if(memory == null){
+            Memory memorySlot1 = new Memory;
+            memory.add(memorySlot1);
+            i++;
+        }else if(i == 1){
+            Memory memorySlot1 = new Memory;
+            memory.add(memorySlot1);
+            i++;
+        }else if(i == 2){
+            return;
+        }
+    }
 }
