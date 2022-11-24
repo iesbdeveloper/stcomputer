a70e9b5bc7b70fddf1bcfbec82c294b83b7c305d
Bruno Lopes Bellinazo <bellinazobl@gmail.com>
2022-11-24 20:05:20 -0300

    Cpu connect and controller

--- src/main/java/inc/hardware/motherboard/chipset/m1/CpuController.java
+++ src/main/java/inc/hardware/motherboard/chipset/m1/CpuController.java
@@ -1,4 +1,23 @@
 package inc.hardware.motherboard.chipset.m1;
 
+import inc.hardware.interfaces.LGA;
+
 public class CpuController {
+
+    public static LGA cpu1;
+
+    public static void connectCpu(LGA peripheral){
+
+        if(cpu1 == null){
+            cpu1 = peripheral;
+        }
+
+    }
+
+    public static void removeCpu(){
+
+        cpu1 = null;
+
+    }
+
 }
